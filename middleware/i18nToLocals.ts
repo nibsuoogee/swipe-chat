1c4eebbda8529d2892288e11d32db22cff671a85
nibsuoogee <elias.syyrila@gmail.com>
2024-03-04 09:58:37 +0200

    more comments

--- middleware/i18nToLocals.ts
+++ middleware/i18nToLocals.ts
@@ -1,6 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import i18next from '../i18n.js';
 
+/*
+* i18nToLocals is used as a middleware function to add the 
+* i18next translation function from i18n.js to res.locals
+* which can be accessed at any point later in the request chain.
+*
+* This is useful for using the translation function in .pug files 
+* when using the res.render() function. Unfortunately, it does
+* not work with pug.compileFile().
+*/
 export default function i18nToLocals(req: Request, res: Response,
   next: NextFunction) {
   res.locals.t = i18next.t;
--- routes/index.ts
+++ routes/index.ts
@@ -8,6 +8,12 @@ import {
 } from '../middleware/checkAuth.js';
 import i18next from '../i18n.js';
 
+/*
+* Apart from the auth middleware, / is the only route which
+* supplies an entire html page (index.pug). Other routes 
+* return markup (html snippets which HTMX uses to insert into 
+* selected places in the DOM).
+*/
 router.get('/', checkAuthReturnIndex, function (req, res) {
   const user = req.user as IUser | null;
   if (user && user.user_name) {
@@ -17,12 +23,22 @@ router.get('/', checkAuthReturnIndex, function (req, res) {
   }
 });
 
+/*
+* /menu return the navigation items for authenticated users
+* checkAuthReturnNothing will mean it returns no markup
+* if authentication fails.
+*/
 router.get('/menu', checkAuthReturnNothing, function (req, res, next) {
   let template = pug.compileFile('views/menu.pug');
   let markup = template({ t: i18next.t });
   return res.send(markup);
 });
 
+/*
+* /lang/:id changes the language of the application to the 
+* identifier (en, fi). Change takes effect on reload, so redirects
+* to /
+*/
 router.post('/lang/:id', async function (req, res, next) {
   await i18next.changeLanguage(req.params.id);
   res.set('HX-Redirect', '/');
